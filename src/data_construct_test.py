ce372c174201a1e1def6fcb7aad37a56c3136475
zhangxiaoyuan <624518625@qq.com>
2016-04-26 20:15:08 +0800

    Update data_construct_test.py

--- src/data_construct_test.py
+++ src/data_construct_test.py
@@ -1,1 +1,17 @@
 
+#!usr/bin/env python 
+#coding=utf-8 
+
+print 'this file is test for python data construct!!!!' 
+ 
+list = ["p",'pp',"ppp", 1,123,232,"234"] 
+ 
+print list 
+print list[2] 
+print list[:5] 
+print list[2:4] 
+
+del list[1] 
+print list 
+print list[1]
+

6aa86657ef54755d4a5d04c0e99d5f36fa768c31
zhangxiaoyuan <624518625@qq.com>
2016-11-13 16:57:50 +0800

    Update functional_programming.py

--- note/functional_programming.py
+++ note/functional_programming.py
@@ -101,3 +101,78 @@ race({'time': 4,
 # 3）在函数里没有临时变量。
 # 4）我们还可以看到，for循环被递归取代了（见race函数）—— 递归是函数式编程中带用到的技术，正如前面所说的，递归的本质就是描述问题是什么
  
+#pipeline:
+#正常的普通函数编写：
+def process(num):
+    # filter out non-evens
+    if num % 2 != 0:
+        return
+    num = num * 3
+    num = 'The Number: %s' % num
+    return num
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for num in nums:
+    print process(num)
+
+# 输出：
+# The Number: 6
+# The Number: 12
+# The Number: 18
+# The Number: 24
+# The Number: 30
+
+#使用pipeline方式编写：
+def even_filter(nums):
+    for num in nums:
+        if num % 2 == 0:
+            yield num
+def multiply_by_three(nums):
+    for num in nums:
+        yield num * 3
+def convert_to_string(nums):
+    for num in nums:
+        yield 'The Number: %s' % num
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+pipeline = convert_to_string(multiply_by_three(even_filter(nums)))
+for num in pipeline:
+    print num
+    
+#我们动用了Python的关键字 yield，这个关键字主要是返回一个Generator，yield 是一个类似 return 的关键字，
+#只是这个函数返回的是个Generator-生成器。所谓生成器的意思是，yield返回的是一个可迭代的对象，并没有真正的执行函数。
+#也就是说，只有其返回的迭代对象被真正迭代时，yield函数才会正真的运行，运行到yield语句时就会停住，然后等下一次的迭代。
+#（这个是个比较诡异的关键字）这就是lazy evluation
+
+#使用Map & Reduce，不要使用循环
+def even_filter(nums):
+    return filter(lambda x: x%2==0, nums)
+
+def multiply_by_three(nums):
+    return map(lambda x: x*3, nums)
+
+def convert_to_string(nums):
+    return map(lambda x: 'The Number: %s' % x,  nums)
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+pipeline = convert_to_string(
+               multiply_by_three(
+                   even_filter(nums)
+               )
+            )
+for num in pipeline:
+    print num
+
+#但是他们的代码需要嵌套使用函数，这个有点不爽，如果我们能像下面这个样子就好了。
+
+pipeline_func(nums, [even_filter,
+                     multiply_by_three,
+                     convert_to_string])
+
+#那么，pipeline_func 实现如下：
+
+def pipeline_func(data, fns):
+    return reduce(lambda a, x: x(a),
+                  fns,
+                  data)
